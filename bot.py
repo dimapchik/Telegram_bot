de8b86b076a20ece80ebbdee179ced744e84dc61
Dmitri Prokopev <Dima02112003Dima@yandex.ru>
2023-04-26 17:18:29 +0300

    add buttons and take api

--- bot.py
+++ bot.py
@@ -7,21 +7,40 @@ bot = telebot.TeleBot(token)
 weather_api = "041a995e39ea3d7536ed0d43eab777c7"
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['help'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет, хочешь узнать погоду?")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    location = types.KeyboardButton('location')
+    temperature = types.KeyboardButton('temperature')
+    photo = types.KeyboardButton('photo location')
+    markup.add(location, temperature, photo)
+    bot.send_message(message.chat.id, "HEEEEEEEEEEELP", reply_markup=markup)
 
 
-@bot.message_handler()
+@bot.message_handler(commands=['weather'])
+def weather(message):
+    bot.send_message(message.chat.id, "Введите название города.")
+
+
+@bot.message_handler(content_types=['text'])
 def request(message):
+    if message.text == "location" or message.text == "temperature":
+        bot.send_message(message.chat.id, "Введите название города")
+        return
     city = message.text.lower().strip()
     data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api}&units=metric')
     weather_info = json.loads(data.text)
-    temp = weather_info["main"]["temp"]
-    lat = weather_info["coord"]["lat"]
-    lon = weather_info["coord"]["lon"]
-    bot.send_message(message.chat.id, f'Погода в городе {city}: {temp}')
-    bot.send_location(message.chat.id, lat, lon)
+    if weather_info["cod"] != 200:
+        bot.send_message(message.chat.id, "Я не знаю такого города")
+    else:
+        lat = weather_info["coord"]["lat"]
+        lon = weather_info["coord"]["lon"]
+        temp = weather_info["main"]["temp"]
+        if message.text == "location":
+            bot.send_location(message.chat.id, lat, lon)
+        if message.text == "temperature":
+            bot.send_message(message.chat.id, f'Текущая температора в городе {city} : {temp}')
+
 
 
 bot.polling(none_stop=True)
