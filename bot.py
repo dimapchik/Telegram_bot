85a414dfc6518329cf7cea0a568bac42e1f70090
Dmitri Prokopev <Dima02112003Dima@yandex.ru>
2023-04-28 15:27:10 +0300

    execute bad requests

--- bot.py
+++ bot.py
@@ -26,34 +26,46 @@ def send_particular_time(callback_query):
     bot.register_next_step_handler(callback_query.message, send_difference)
 
 
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    location_button = types.KeyboardButton('location')
-    weather_button = types.KeyboardButton('weather')
-    airport_button = types.KeyboardButton('airport')
-    time_button = types.KeyboardButton('time')
+    location_button = types.KeyboardButton('/location')
+    weather_button = types.KeyboardButton('/weather')
+    airport_button = types.KeyboardButton('/airport')
+    time_button = types.KeyboardButton('/time')
     markup.add(location_button, weather_button, airport_button, time_button)
     bot.send_message(message.chat.id, "Hi, I'm GeoTimingBot. I can send location by name of city (for it you need "
-                                      "write 'location'), send particular weather in city (write 'weather'), "
-                                      "also I can send you list of airports in your city (write 'airports'), also send "
-                                      "you current time in city or difference time in cities",
+                                      "write /location), send particular weather in city (write /weather), "
+                                      "also I can send you list of airports in your city (write /airports), also send "
+                                      "you current time in city or difference time in cities (write /time). "
+                                      "For squalor you can use buttons under your keyboard",
                      reply_markup=markup)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['help'])
+def help_request(message):
+    bot.send_message(message.chat.id, "/help - this message \n"
+                                      "/start - starting message and reset buttons \n"
+                                      "/location - get location by name of city \n"
+                                      "/weather - get particular weather by name of city \n"
+                                      "/airports - get list of airports in city \n"
+                                      "/time - get current time by name of city or get time difference between two"
+                                      "cities")
+
+
+@bot.message_handler(commands=['location', 'weather', 'airports', 'time'])
 def request(message):
-    if message.text == "location":
+    if message.text == "/location":
         bot.send_message(message.chat.id, "Write name of city")
         bot.register_next_step_handler(message, send_location)
-    elif message.text == "weather":
+    elif message.text == "/weather":
         bot.send_message(message.chat.id, "Write name of city")
         bot.register_next_step_handler(message, send_weather)
-    elif message.text == "airport":
+    elif message.text == "/airports":
         bot.send_message(message.chat.id, "Write name of city and short name of country (ex. London GB,"
                                           " or San Francisco US)")
         bot.register_next_step_handler(message, send_airport)
-    elif message.text == "time":
+    elif message.text == "/time":
         markup = types.InlineKeyboardMarkup()
         time_in_city_button = types.InlineKeyboardButton("time in a particular city",
                                                          callback_data="time in a particular city")
@@ -61,6 +73,17 @@ def request(message):
                                                             callback_data="time difference between two cities")
         markup.add(time_in_city_button, time_difference_button)
         bot.send_message(message.chat.id, "What kind of time option do you want?", reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def text_request(message):
+    bot.send_message(message.chat.id, "I don't know this command. You can write /help or use buttons "
+                                      "under your keyboard")
+
+
+@bot.message_handler()
+def bad_request(message):
+    bot.send_message(message.chat.id, "I can't handle any types is not a text(")
 
 
 def send_location(message):
@@ -106,7 +129,8 @@ def send_airport(message):
 def send_time(message):
     city = message.text.lower().strip()
     weather_info = execute_weather_request(city)
-    bot.send_message(message.chat.id, get_time(message, weather_info).strftime('%H:%M'))
+    if check_weather_existing(message, weather_info):
+        bot.send_message(message.chat.id, get_time(message, weather_info).strftime('%H:%M'))
 
 
 def send_difference(message):
